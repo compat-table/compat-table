633bbb5efe464e9973dc0728b8a13b6853c64c48
Rod Vagg <rod@vagg.org>
2012-03-16 10:30:08 +1100

    updated to latest domReady

--- ready.js
+++ ready.js
@@ -1,3 +1,6 @@
+/*!
+  * domready (c) Dustin Diaz 2012 - License MIT
+  */
 !function (name, definition) {
   if (typeof module != 'undefined') module.exports = definition()
   else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
@@ -11,12 +14,12 @@
     , domContentLoaded = 'DOMContentLoaded'
     , addEventListener = 'addEventListener'
     , onreadystatechange = 'onreadystatechange'
-    , loaded = /^loade|c/.test(doc.readyState)
+    , readyState = 'readyState'
+    , loaded = /^loade|c/.test(doc[readyState])
 
   function flush(f) {
     loaded = 1
-    while (f = fns.shift())
-      try { f() } catch (e) {}
+    while (f = fns.shift()) f()
   }
 
   doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
@@ -25,12 +28,12 @@
   }, f)
 
 
-  hack && doc.attachEvent(onreadystatechange, (fn = function () {
-    if (/^c/.test(doc.readyState)) {
+  hack && doc.attachEvent(onreadystatechange, fn = function () {
+    if (/^c/.test(doc[readyState])) {
       doc.detachEvent(onreadystatechange, fn)
       flush()
     }
-  }))
+  })
 
   return (ready = hack ?
     function (fn) {
