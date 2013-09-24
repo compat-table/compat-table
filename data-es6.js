fea3439731dd11fc0f417b880c30911773d64419
fbender <fb+git@quantumedia.de>
2013-09-24 18:31:43 +0200

    Mark obsolete engine versions in ES6 compat table.
    All engine versions that are no longer officially maintained (at time
    of commit) are marked as obsolete in the ES6 compatibility table. When
    an engine is marked as obsolete, the "obsolete" class is added to each
    respective table cell when building the output HTML file.

--- data-es6.js
+++ data-es6.js
@@ -7,7 +7,8 @@ exports.skeleton_file = 'es6/skeleton.html';
 exports.browsers = {
   ie10: {
     full: 'Internet Explorer',
-    short: 'IE 10'
+    short: 'IE 10',
+    obsolete: false // no EOL any time soon
   },
   ie11: {
     full: 'Internet Explorer',
@@ -15,31 +16,38 @@ exports.browsers = {
   },
   firefox11: {
     full: 'Firefox',
-    short: 'FF 11, 12'
+    short: 'FF 11, 12',
+    obsolete: true
   },
   firefox13: {
     full: 'Firefox',
-    short: 'FF 13'
+    short: 'FF 13',
+    obsolete: true
   },
   firefox16: {
     full: 'Firefox',
-    short: 'FF 16'
+    short: 'FF 16',
+    obsolete: true
   },
   firefox17: {
     full: 'Firefox',
-    short: 'FF 17'
+    short: 'FF 17',
+    obsolete: false // ESR, EOL 2013-12
   },
   firefox18: {
     full: 'Firefox',
-    short: 'FF 18'
+    short: 'FF 18',
+    obsolete: true
   },
   firefox23: {
     full: 'Firefox',
-    short: 'FF 23'
+    short: 'FF 23',
+    obsolete: true
   },
   firefox24: {
     full: 'Firefox',
-    short: 'FF 24'
+    short: 'FF 24',
+    obsolete: false // ESR (EOL at end of 2014)
   },
   firefox25: {
     full: 'Firefox',
@@ -51,17 +59,20 @@ exports.browsers = {
   },
   chrome: {
     full: 'Chrome',
-    short: 'CH &lt;19'
+    short: 'CH &lt;19',
+    obsolete: true
   },
   chrome19dev: {
     full: 'Chrome',
     short: 'CH 19',
+    obsolete: true,
     note_id: 'experimental-flag',
     note_html: 'Have to be enabled via "Experimental Javascript features" flag'
   },
   chrome21dev: {
     full: 'Chrome',
     short: 'CH 21-29',
+    obsolete: false, // soon EOL
     note_id: 'experimental-flag',
     note_html: 'Have to be enabled via "Experimental Javascript features" flag'
   },
@@ -73,19 +84,23 @@ exports.browsers = {
   },
   safari51: {
     full: 'Safari',
-    short: 'SF 5.1'
+    short: 'SF 5.1',
+    obsolete: true
   },
   safari6: {
     full: 'Safari',
-    short: 'SF 6'
+    short: 'SF 6',
+    obsolete: false // soon EOL
   },
   webkit: {
     full: 'WebKit',
-    short: 'WK'
+    short: 'WK',
+    obsolete: false // version unknown
   },
   opera: {
     full: 'Opera 12',
-    short: 'OP 12'
+    short: 'OP 12',
+    obsolete: false // still supported
   },
   opera15: {
     full: 'Opera 15.0',
