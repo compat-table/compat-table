2e63139c0e512ef2ea70d2abc4b96a2b2bdf130b
Leon <leon@Leons-iMac.local>
2014-07-04 13:00:38 +1000

    Destructuring test now has object destructuring

--- data-es6.js
+++ data-es6.js
@@ -1660,7 +1660,7 @@ exports.tests = [
   exec: function () {
     'use strict';
     try {
-      eval('var [a] = [5];');
+      eval('var [a,b] = [5,6], {c,d} = {c:7,d:8}; a === 5 && b === 6 && c === 7 && d === 8;');
       return true;
     } catch (e) {
       return false;
