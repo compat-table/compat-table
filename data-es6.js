4bc5a4a3a3c1d8345d5451f3607f55427f3df852
Leon <L_1_L_0@yahoo.com>
2015-07-24 09:26:23 +1000

    Added Typed Array 'correct prototype chain' subtest.

--- data-es6.js
+++ data-es6.js
@@ -3654,6 +3654,37 @@ exports.tests = [
         firefox11:false,
       }),
     },
+    'correct prototype chains': {
+      exec: function(){/*
+        var constructors = [
+          'Int8Array',
+          'Uint8Array',
+          'Uint8ClampedArray',
+          'Int16Array',
+          'Uint16Array',
+          'Int32Array',
+          'Uint32Array',
+          'Float32Array',
+          'Float64Array'
+        ];
+        var constructor = Object.getPrototypeOf(Int8Array);
+        var prototype = Object.getPrototypeOf(Int8Array.prototype);
+        for(var i = 0; i < constructors.length; i+=1) {
+          if (!(constructors[i] in global
+              && Object.getPrototypeOf(global[constructors[i]]) === constructor
+              && Object.getPrototypeOf(global[constructors[i]].prototype) === prototype
+              && Object.getOwnPropertyNames(global[constructors[i]].prototype).sort() + ''
+                === "BYTES_PER_ELEMENT,constructor")) {
+            return false;
+          }
+        }
+        return true;
+      */},
+      res: {
+        firefox35:   true,
+        edge:        true,
+      },
+    },
   },
   (function(){
     var methods = {
