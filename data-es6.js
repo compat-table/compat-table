fd422278a7953091bacaa42ee8bb1e85a4568fd1
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-15 00:21:27 +0600

    [ES6] Add test "Typed arrays constructors accepts generic iterables"

--- data-es6.js
+++ data-es6.js
@@ -4031,6 +4031,30 @@ exports.tests = [
         firefox11:false,
       }),
     },
+    'constructors accepts generic iterables': {
+      exec: function(){/*
+        var constructors = [
+          'Int8Array',
+          'Uint8Array',
+          'Uint8ClampedArray',
+          'Int16Array',
+          'Uint16Array',
+          'Int32Array',
+          'Uint32Array',
+          'Float32Array',
+          'Float64Array'
+        ];
+        for(var i = 0; i < constructors.length; i++){
+          var arr = new global[constructors[i]](__createIterableObject([1, 2, 3]));
+          if(arr.length !== 3 || arr[0] !== 1 || arr[1] !== 2 || arr[2] !== 3)return false;
+        }
+        return true;
+      */},
+      res: {
+        chrome45:    true,
+        node4:       true,
+      },
+    },
     'correct prototype chains': {
       exec: function(){/*
         var constructors = [
