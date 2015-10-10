0f066871005b0d79272f92763d3586519a19a740
Claude Pache <claude.pache@gmail.com>
2015-10-10 21:03:51 +0200

    Check that Error.prototype is not an Error object
    Test: "built-in prototypes are not instances".
    Add checks for Error and TypeError.

--- data-es6.js
+++ data-es6.js
@@ -11808,6 +11808,10 @@ exports.tests = [
         try {
           Date.prototype.valueOf(); return false;
         } catch(e) {}
+        if (Object.prototype.toString.call(Error.prototype)  !== '[object Object]')
+          return false;
+        if (Object.prototype.toString.call(TypeError.prototype) !== '[object Object]')
+          return false;
         return true;
       */},
       res: {
