e8eabc63ab646931bd85576746a9b82ef8012af0
Leon <L_1_L_0@yahoo.com>
2016-01-30 22:31:50 +1000

    [ES7] Added proxy test for Array#includes.
    Proxy tests should probably only be added for Stage 4 features, given they hinge on specific implementation details.

--- data-es7.js
+++ data-es7.js
@@ -481,6 +481,24 @@ exports.tests = [
         firefox43:       true,
       }
     },
+    {
+      name: 'internal \'Get\' calls',
+      exec: function() {/*
+        // Array.prototype.includes -> Get -> [[Get]]
+        var get = [];
+        var p = new Proxy({length: 3, 0: '', 1: '', 2: '', 3: ''}, { get: function(o, k) { get.push(k); return o[k]; }});
+        Array.prototype.includes.call(p, {});
+        if (get + '' !== "length,0,1,2") return;
+
+        get = [];
+        p = new Proxy({length: 4, 0: NaN, 1: '', 2: NaN, 3: ''}, { get: function(o, k) { get.push(k); return o[k]; }});
+        Array.prototype.includes.call(p, NaN, 1);
+        return (get + '' === "length,1,2");
+      */},
+      res: {
+        firefox43:       true,
+      },
+    }
   ],
 },
 {
