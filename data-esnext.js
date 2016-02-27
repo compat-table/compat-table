20aaf8b0e6cb4fa51f38891a17cde7f7eb3a9c92
Denis Pushkarev <zloirock@zloirock.ru>
2016-02-27 16:14:42 +0600

    [ESNext] Add String#matchAll test

--- data-esnext.js
+++ data-esnext.js
@@ -1576,6 +1576,27 @@ exports.tests = [
       }
     }
   ]
+},
+{
+  name: 'String.prototype.matchAll',
+  category: 'proposal (stage 1)',
+  significance: 'small',
+  link: 'https://github.com/tc39/String.prototype.matchAll',
+  exec: function(){/*
+    var iterator = '11a2bb'.matchAll(/(\d)(\D)/);
+    if(iterator[Symbol.iterator]() !== iterator)return false;
+    var a = '', b = '', c = '', step;
+    while(!(step = iterator.next()).done){
+      a += step.value[0];
+      b += step.value[1];
+      c += step.value[2];
+    }
+    return a === '1a2b'
+      && b === '12'
+      && c === 'ab';
+  */},
+  res: {
+  }
 }
 ];
 
