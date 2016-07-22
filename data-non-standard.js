32d67cd1707771b56bbade6d012c7e538e198395
Claude Pache <claude.pache@gmail.com>
2016-07-22 16:04:06 +0200

    Correction: IE10 on WinVista is not obsolete

--- data-non-standard.js
+++ data-non-standard.js
@@ -9,7 +9,7 @@ exports.browsers = {
     full: 'Internet Explorer 7-10',
     short: 'IE 7-10',
     family: 'Chakra',
-    obsolete: true
+    obsolete: false
   },
   ie11: {
     full: 'Internet Explorer 11',
