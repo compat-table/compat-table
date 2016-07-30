1e7dfaeef6d05944b3f9c9f51742809c6d2eaf52
Leon <L_1_L_0@yahoo.com>
2016-07-30 14:53:16 +1000

    [ES2016+] Added "Object.getOwnPropertyDescriptors doesn't provide undefined descriptors" subtest.

--- data-es2016plus.js
+++ data-es2016plus.js
@@ -243,26 +243,42 @@ exports.tests = [
     link: 'https://github.com/tc39/proposal-object-getownpropertydescriptors',
     category: '2017 features',
     significance: 'small',
-    exec: function () {/*
-     var object = {a: 1};
-     var B = typeof Symbol === 'function' ? Symbol('b') : 'b';
-     object[B] = 2;
-     var O = Object.defineProperty(object, 'c', {value: 3});
-     var D = Object.getOwnPropertyDescriptors(O);
+    subtests: [
+      {
+        name: 'basic support',
+        exec: function () {/*
+          var object = {a: 1};
+          var B = typeof Symbol === 'function' ? Symbol('b') : 'b';
+          object[B] = 2;
+          var O = Object.defineProperty(object, 'c', {value: 3});
+          var D = Object.getOwnPropertyDescriptors(O);
 
-     return D.a.value === 1 && D.a.enumerable === true && D.a.configurable === true && D.a.writable === true
-     && D[B].value === 2 && D[B].enumerable === true && D[B].configurable === true && D[B].writable === true
-     && D.c.value === 3 && D.c.enumerable === false && D.c.configurable === false && D.c.writable === false;
-     */},
-    res: {
-      babel:       true,
-      es7shim:     true,
-      typescript:  typescript.corejs,
-      chrome51:    flag,
-      safari10:    true,
-      safaritp:    true,
-      webkit:      true,
-    }
+          return D.a.value === 1 && D.a.enumerable === true && D.a.configurable === true && D.a.writable === true
+          && D[B].value === 2 && D[B].enumerable === true && D[B].configurable === true && D[B].writable === true
+          && D.c.value === 3 && D.c.enumerable === false && D.c.configurable === false && D.c.writable === false;
+          */},
+        res: {
+          babel:       true,
+          es7shim:     true,
+          typescript:  typescript.corejs,
+          chrome51:    flag,
+          safari10:    true,
+          safaritp:    true,
+          webkit:      true,
+        },
+      },
+      {
+        name: "doesn't provide undefined descriptors",
+        exec: function () {/*
+          var P = new Proxy({a:1}, {
+            getOwnPropertyDescriptor: function(t, k) {}
+          });
+          return !Object.getOwnPropertyDescriptors(P).hasOwnProperty('a');
+        */},
+        res: {
+        },
+      },
+    ]
   },
   {
     name: 'String padding',
