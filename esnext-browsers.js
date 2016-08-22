2ba374f6a515de94b0379c5b9d9895d523e71241
Yusuke Suzuki <utatane.tea@gmail.com>
2016-08-22 16:08:49 -0700

    Update WebKit to r204705

--- esnext-browsers.js
+++ esnext-browsers.js
@@ -305,7 +305,7 @@ module.exports = {
       unstable: true
   },
   webkit: {
-    full: 'WebKit r204466 (August 15, 2016)',
+    full: 'WebKit r204705 (August 22, 2016)',
       short: 'WK',
       unstable: true,
   },
