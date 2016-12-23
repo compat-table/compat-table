8e0acd99d9b050675d6a9d2d923633d879939cd9
Jordan Harband <ljharb@gmail.com>
2016-12-23 13:48:46 -0800

    [esnext] Fix typo from #991

--- data-esnext.js
+++ data-esnext.js
@@ -850,7 +850,7 @@ exports.tests = [
       if (typeof global !== 'object') { return false; }
       if (!('global' in actualGlobal)) { return false; }
       if (Object.prototype.propertyIsEnumerable.call(actualGlobal, 'global')) { return false; }
-      if (typeof Object.getOwnPropertyDescriptor !== 'function) { return true; } // ES3
+      if (typeof Object.getOwnPropertyDescriptor !== 'function') { return true; } // ES3
 
       var descriptor = Object.getOwnPropertyDescriptor(actualGlobal, 'global');
       return descriptor.value === actualGlobal && !descriptor.enumerable && descriptor.configurable && descriptor.writable;
