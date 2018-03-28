d122557cef980377aaab27f3973f2246899d50c6
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-28 23:53:40 +0700

    [ESNext] Add `Set.prototype.map`

--- data-esnext.js
+++ data-esnext.js
@@ -3637,6 +3637,18 @@ exports.tests = [
       res: {
       }
     },
+    {
+      name: 'Set.prototype.map',
+      exec: function () {/*
+        var set = new Set([1, 2, 3]).map(it => it * it);
+        return set.size === 3
+          && set.has(1)
+          && set.has(4)
+          && set.has(9);
+      */},
+      res: {
+      }
+    },
   ]
 },
 ];
