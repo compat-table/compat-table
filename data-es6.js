0fd8253b87b674f6ee8b503a5d9ef6567e324f8a
Alexandre Folle de Menezes <Alexandre.Menezes@corp.terra.com>
2018-06-12 14:00:15 -0300

    Update the notes for TypedArray tests

--- data-es6.js
+++ data-es6.js
@@ -6324,14 +6324,14 @@ exports.tests = [
       res: {
         babel6corejs2: babel.corejs,
         typescript1corejs2: typescript.corejs,
-        firefox2: false,
-        firefox4: true,
         ie11: {
           val: true,
           note_id: "ie-clampedarray",
-          note_html: "<code>Uint8ClampedArray</code> has been introduced in the build <a href='https://support.microsoft.com/en-us/help/2929437/description-of-the-security-update-for-internet-explorer-11-on-windows'>KB2929437</a>, so it is missing in early IE11 builds"
+          note_html: "A partial implementation of <code>Uint8ClampedArray</code> has been introduced with <a href='https://support.microsoft.com/en-us/help/2929437/description-of-the-security-update-for-internet-explorer-11-on-windows'>KB2929437</a>, so this test fails on early IE11 builds."
         },
         edge12: true,
+        firefox2: false,
+        firefox4: true,
         chrome5: true,
         safari6: true,
         opera10_50: false,
@@ -7038,7 +7038,11 @@ exports.tests = [
       babel6corejs2: babel.corejs,
       typescript1corejs2: typescript.corejs,
       ejs: true,
-      ie11: true,
+      ie11: {
+        val: true,
+        note_id: "ie-subarray",
+        note_html: "This test depends on <code>Uint8ClampedArray</code> which has been introduced with <a href='https://support.microsoft.com/en-us/help/2929437/description-of-the-security-update-for-internet-explorer-11-on-windows'>KB2929437</a>, so it fails on early IE11 builds."
+      },
       edge12: true,
       firefox2: false,
       firefox4: true,
