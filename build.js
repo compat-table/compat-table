0933371cabc41ee052db4e33d38c674bdf6d5f46
Christopher Darroch <christopher.darroch@gmail.com>
2018-12-27 14:10:04 +0100

    refactor: pull in test parser from external module.

--- build.js
+++ build.js
@@ -24,6 +24,8 @@ require('object.assign').shim();
 var pickBy = require('lodash.pickby');
 
 var environments = require('./environments');
+var parser = require('./test-parser');
+var prepareTest = parser.prepareTest;
 
 var fs = require('fs');
 var path = require('path');
@@ -746,93 +748,4 @@ function replaceAndIndent(str, replacements) {
       .join(replacement[1].join(indent[1]));
   }
   return str;
-}
-
-function prepareTest(fn, transformFn, rowNum) {
-  var script, expr;
-  function deindentFunc(fn) {
-    fn = (fn+'');
-    var indent = /(?:^|\n)([\t ]+)[^\n]+/.exec(fn);
-    if (indent) {
-      fn = fn.replace(new RegExp('\n' + indent[1], 'g'), '\n');
-    }
-    return fn;
-  }
-
-  if (!fn) {
-    return { script: script, source: expr };
-  }
-  if (typeof fn === 'function') {
-    // see if the code is encoded in a comment
-    expr = (fn+"").match(/[^]*\/\*([^]*)\*\/\}$/);
-    var transformed = false;
-    // if there wasn't an expression, make the function statement into one
-    if (!expr) {
-      if (transformFn) {
-        try {
-          expr = transformFn("("+fn+")");
-          transformed = true;
-        } catch(e) {
-          expr = "false";
-        }
-      }
-      else {
-        expr = deindentFunc(fn);
-      }
-      script = 'test(\n' + expr + '())';
-      return { script: script, source: expr };
-    }
-    else {
-      expr = deindentFunc(expr[1]);
-      if (transformFn) {
-        try {
-          if (expr.search(/Function\s*\(|eval\s*\(/) > -1) {
-            throw new Error("This test's code invokes eval() and cannot be compiled.");
-          }
-          expr = transformFn("(function(){" + expr + "})");
-          transformed = true;
-        } catch(e) {
-          expr = "/* Error during compilation: " + e.message + "*/";
-        }
-      }
-      /* jshint unused: true */ // this appears unused, but removing it might break things.
-      var async = !!/asyncTestPassed/.exec(fn);
-      /* jshint unused: false */
-      var codeString = JSON.stringify(expr).replace(/\\r/g,'');
-      var asyncFn = 'global.__asyncPassedFn && __asyncPassedFn("' + rowNum + '")';
-      var strictAsyncFn = 'global.__strictAsyncPassedFn && __strictAsyncPassedFn("' + rowNum + '")';
-      var funcString =
-        transformed ? '' + asyncFn + ' && eval(' + codeString + ')()'
-        : 'Function("asyncTestPassed",' + codeString + ')(asyncTestPassed)';
-      var strictFuncString =
-        transformed ? '' + strictAsyncFn + ' && function(){"use strict";' + codeString + '}() && "Strict"'
-        : 'Function("asyncTestPassed","\'use strict\';"+' + codeString + ')(asyncTestPassed)';
-
-      script = 'test(function(){'
-        +  'try{'
-        +    'var asyncTestPassed=' + asyncFn + ';'
-        +    'try{'
-        +      'return ' + funcString
-        +    '}'
-        +    'catch(e){'
-        +      'asyncTestPassed=' + strictAsyncFn + ';'
-        +      'return ' + strictFuncString + '&&"Strict"'
-        +    '}'
-        +  '}'
-        +  'catch(e){'
-        +    'return false;'
-        +  '}'
-        +'}());';
-      return { script: script, source: expr };
-    }
-  } else {
-    // it's an array of objects like the following:
-    // { type: 'application/javascript;version=1.8', script: function () { ... } }
-    return fn.map(function(script) {
-      var expr = deindentFunc(
-          (script.script+'').replace(/^function \(\) \{\s*|\s*\}$/g, '')
-        );
-      return { script: expr, source: expr, type: script.type };
-    });
-  }
 }
