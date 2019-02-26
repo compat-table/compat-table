1472f20cad5423a97ee41a0a64a340f999095dfd
Justin Ridgewell <jridgewell@google.com>
2019-02-26 00:57:13 -0500

    Add comment

--- data-es6.js
+++ data-es6.js
@@ -5461,6 +5461,11 @@ exports.tests = [
     {
       name: 'TemplateStrings call site caching',
       exec: function () {/*
+        // Safari 12 sometimes garbage collects the cached TemplateStrings,
+        // even when the call site is still alive.
+        // This is almost impossible to write a test case for, but we can test
+        // the general semantics.
+        // https://bugs.webkit.org/show_bug.cgi?id=190756
         function strings(array) {
           return array;
         }
