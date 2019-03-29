2b1645f4393eb82061c1b6a53c1a451de280af41
Alexandre Folle de Menezes <afmenez@gmail.com>
2019-03-29 01:33:20 -0300

    One more feature promoted

--- data-esnext.js
+++ data-esnext.js
@@ -1884,7 +1884,7 @@ exports.tests = [
 {
   name: 'numeric separators',
   spec: 'https://github.com/tc39/proposal-numeric-separator',
-  category: STAGE2,
+  category: STAGE3,
   significance: 'small',
   exec: function(){/*
     return 1_000_000.000_001 === 1000000.000001 &&
