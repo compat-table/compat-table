960d15598759c05a3956494fa3489424dc4fc694
Alexandre Folle de Menezes <afmenez@gmail.com>
2019-08-12 13:17:03 -0300

    Fixing note text

--- data-esnext.js
+++ data-esnext.js
@@ -1712,7 +1712,7 @@ exports.tests = [
     babel7corejs2: true,
     ie11: false,
     firefox52: false,
-    safaritp: {val: 'flagged', note_id: "safari-nullish", note_html: "You can flip it on like this: <code>__XPC_JSC_useNullishCoalescing=true && open -a 'Safari Technology Preview'</code>."},
+    safaritp: {val: 'flagged', note_id: "safari-nullish", note_html: "The feature have to be enabled via JSC runtime flag: <code>__XPC_JSC_useNullishCoalescing=true open -a 'Safari Technology Preview'</code>."},
     graalvm: false,
   }
 },
