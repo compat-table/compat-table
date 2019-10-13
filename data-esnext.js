dde1a6e0e2b631af880fc9a311a036474c4e0912
Denis Pushkarev <zloirock@zloirock.ru>
2019-10-14 00:41:07 +0700

    Iterator.prototype.filter

--- data-esnext.js
+++ data-esnext.js
@@ -2967,6 +2967,18 @@ exports.tests = [
         typescript3_2corejs3: typescript.corejs,
       },
     },
+    {
+      name: 'Iterator.prototype.filter',
+      exec: function () {/*
+        return Array.from([1, 2, 3].values().filter(it => it % 2)).join() === '1,3';
+      */},
+      res: {
+        babel6corejs2: false,
+        babel7corejs3: babel.corejs,
+        typescript1corejs2: typescript.fallthrough,
+        typescript3_2corejs3: typescript.corejs,
+      },
+    },
   ]
 },
 ];
