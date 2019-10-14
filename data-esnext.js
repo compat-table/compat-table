7bf1de53997d10861bcd9e4ec83da3df40ca5095
Denis Pushkarev <zloirock@zloirock.ru>
2019-10-14 18:56:31 +0700

    Iterator.prototype.some

--- data-esnext.js
+++ data-esnext.js
@@ -3041,6 +3041,18 @@ exports.tests = [
         typescript3_2corejs3: typescript.corejs,
       },
     },
+    {
+      name: 'Iterator.prototype.some',
+      exec: function () {/*
+        return [1, 2, 3].values().some(it => typeof it === 'number');
+      */},
+      res: {
+        babel6corejs2: false,
+        babel7corejs3: babel.corejs,
+        typescript1corejs2: typescript.fallthrough,
+        typescript3_2corejs3: typescript.corejs,
+      },
+    },
   ]
 },
 ];
