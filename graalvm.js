86ead7fa5a2b96b7d9a0fdbc42ff9319ac096953
Jirka Marsik <jiri.marsik@oracle.com>
2022-08-08 12:25:27 +0200

    Downgrade graalvm.js to ES5 in order to pass linter

--- graalvm.js
+++ graalvm.js
@@ -8,18 +8,18 @@
  *    $ node graalvm.js
  */
 
-const fs = require('fs');
-const child_process = require('child_process');
+var fs = require('fs');
+var child_process = require('child_process');
 
-let testCount = 0;
-let testSuccess = 0;
-let testOutOfDate = 0;
+var testCount = 0;
+var testSuccess = 0;
+var testOutOfDate = 0;
 
-const jsCommand = 'js';
+var jsCommand = 'js';
 
-const environments = JSON.parse(fs.readFileSync('environments.json').toString());
+var environments = JSON.parse(fs.readFileSync('environments.json').toString());
 
-const flagsForSuite = {
+var flagsForSuite = {
     'data-es5': [ [ '--js.ecmascript-version=5' ] ],
     'data-es6': [ [ '--js.ecmascript-version=6' ] ],
     'data-es2016plus': [ [ '--js.ecmascript-version=latest' ] ],
@@ -28,60 +28,59 @@ const flagsForSuite = {
     'data-non-standard': [ [ '--experimental-options', '--js.nashorn-compat' ], [ '--experimental-options', '--js.v8-compat' ], [ '--experimental-options', '--js.global-property' ] ]
 };
 
-const prelude = 
-`var __script_executed = {};
-if (typeof global === 'undefined') {
-    this.lacksGlobal = true;
-    global = this;
-}
-if (typeof globalThis === 'undefined') {
-    this.lacksGlobalThis = true;
-    globalThis = this;
-}
-function test(expression) {
-    if (expression) {
-        print("[SUCCESS]");
-    }
-}
-function asyncTestPassed() {
-    print("[SUCCESS]");
-}
-function __createIterableObject(arr, methods) {
-    methods = methods || {};
-    if (typeof Symbol !== 'function' || !Symbol.iterator) {
-        return {};
-    }
-    arr.length++;
-    var iterator = {
-        next: function() {
-            return { value: arr.shift(), done: arr.length <= 0 };
-        },
-        'return': methods['return'],
-        'throw': methods['throw']
-    };
-    var iterable = {};
-    iterable[Symbol.iterator] = function(){ return iterator; }
-    return iterable;
-}
-`;
+var prelude = 
+    'var __script_executed = {};\n' + 
+    'if (typeof global === "undefined") {\n' +
+    '    this.lacksGlobal = true;\n' +
+    '    global = this;\n' +
+    '}\n' +
+    'if (typeof globalThis === "undefined") {\n' +
+    '    this.lacksGlobalThis = true;\n' +
+    '    globalThis = this;\n' +
+    '}\n' +
+    'function test(expression) {\n' +
+    '    if (expression) {\n' +
+    '        print("[SUCCESS]");\n' +
+    '    }\n' +
+    '}\n' +
+    'function asyncTestPassed() {\n' +
+    '    print("[SUCCESS]");\n' +
+    '}\n' +
+    'function __createIterableObject(arr, methods) {\n' +
+    '    methods = methods || {};\n' +
+    '    if (typeof Symbol !== "function" || !Symbol.iterator) {\n' +
+    '        return {};\n' +
+    '    }\n' +
+    '    arr.length++;\n' +
+    '    var iterator = {\n' +
+    '        next: function() {\n' +
+    '            return { value: arr.shift(), done: arr.length <= 0 };\n' +
+    '        },\n' +
+    '        "return": methods["return"],\n' +
+    '        "throw": methods["throw"]\n' +
+    '    };\n' +
+    '    var iterable = {};\n' +
+    '    iterable[Symbol.iterator] = function(){ return iterator; };\n' +
+    '    return iterable;\n' +
+    '}\n';
 
 // Key for .res (e.g. test.res.graalvm), automatic based on GraalVM version.
-const graalvmKey = (function () {
-    const stdout = child_process.execFileSync(jsCommand, [ '--version' ], {
+var graalvmKey = (function () {
+    var stdout = child_process.execFileSync(jsCommand, [ '--version' ], {
         encoding: 'utf-8'
     });
 
-    const version = /[\d\.]+/.exec(stdout)[0];
-    console.log(`GraalVM version is: ${version}`);
+    var version = /[\d\.]+/.exec(stdout)[0];
+    console.log('GraalVM version is: ' + version);
     return version.replace(/\.0(?:$|(?=\.))/g, '').replace(/\./g, '_');
 })();
-console.log(`GraalVM result key is: test.res.graalvm${graalvmKey}`);
+console.log('GraalVM result key is: test.res.graalvm' + graalvmKey);
 
 // List of keys for inheriting results from previous versions.
-const graalvmKeyList = (function () {
-    const res = [];
-    for (const k in environments) {
-        const env = environments[k];
+var graalvmKeyList = (function () {
+    var res = [];
+    for (var k in environments) {
+        var env = environments[k];
         if (env.family !== 'GraalVM') {
             continue;
         }
@@ -96,7 +95,7 @@ const graalvmKeyList = (function () {
 console.log('GraalVM key list for inheriting results is:', graalvmKeyList);
 
 function executeTestScript(testScriptFilename, suite) {
-    let stdout = child_process.execFileSync(jsCommand, [ testScriptFilename ], {
+    var stdout = child_process.execFileSync(jsCommand, [ testScriptFilename ], {
         encoding: 'utf-8'
     });
 
@@ -104,8 +103,9 @@ function executeTestScript(testScriptFilename, suite) {
         return true;
     }
 
-    for (const flags of flagsForSuite[suite]) {
-        const args = flags.slice(0);
+    for (var i = 0; i < flagsForSuite[suite].length; i++) {
+        var flags = flagsForSuite[suite][i];
+        var args = flags.slice(0);
         args.push(testScriptFilename);
         stdout = child_process.execFileSync(jsCommand, args, {
             encoding: 'utf-8'
@@ -114,8 +114,8 @@ function executeTestScript(testScriptFilename, suite) {
         if (/^\[SUCCESS\]$/m.test(stdout)) {
             return {
                 val: 'flagged',
-                note_id: `graalvm-${flags.join('-')}`.replace(/[=\.]/g, '-').replace(/-+/g, '-'),
-                note_html: `Requires the <code>${flags.join(' ')}</code> flag${flags.length > 1 ? 's' : ''}.`
+                note_id: ('graalvm-' + flags.join('-')).replace(/[=\.]/g, '-').replace(/-+/g, '-'),
+                note_html: 'Requires the <code>' + flags.join(' ') + '</code> flag' + flags.length > 1 ? 's' : '' + '.'
             };
         }
     }
@@ -135,16 +135,16 @@ function resultsMatch(a, b) {
 
 function testCode(exec) {
     if (typeof exec === 'function') {
-        const src = exec.toString();
-        const functionBody = /^function\s*\w*\s*\(.*?\)\s*\{\s*\/\*([\s\S]*?)\*\/\s*\}$/m.exec(src);
+        var src = exec.toString();
+        var functionBody = /^function\s*\w*\s*\(.*?\)\s*\{\s*\/\*([\s\S]*?)\*\/\s*\}$/m.exec(src);
         if (functionBody) {
-            const indentation = /^[\t ]+/m.exec(functionBody[1]);
-            return `(function () { ${functionBody[1].replace(new RegExp('^' + indentation[0], 'gm'), '')} })();`;
+            var indentation = /^[\t ]+/m.exec(functionBody[1]);
+            return '(function () { ' + functionBody[1].replace(new RegExp('^' + indentation[0], 'gm'), '') + ' })();';
         } else {
-            return `(${src})()`;
+            return '(' + src + ')()';
         }
     } else if (Array.isArray(exec)) {
-        return exec.map(e => testCode(e.script)).join(" || ");
+        return exec.map(function (e) { return testCode(e.script); }).join(" || ");
     } else {
         return undefined;
     }
@@ -153,27 +153,26 @@ function testCode(exec) {
 // Run test / subtests, recursively.  Report results, indicate data files
 // which are out of date.
 function runTest(parents, test) {
-    const testPath = parents.join(' -> ') + ' -> ' + test.name;
+    var testPath = parents.join(' -> ') + ' -> ' + test.name;
 
-    let evalcode = testCode(test.exec);
+    var evalcode = testCode(test.exec);
     if (evalcode !== undefined) {
         if (evalcode.match(/\bsetTimeout\b/)) {
-            console.log(`${testPath}: could not run test using setTimeout`);
+            console.log(testPath + ': could not run test using setTimeout');
         } else {
-            const script =
-`${prelude}
-var evalcode = ${JSON.stringify(evalcode)};
-try {
-    var result = eval(evalcode);
-    if (result) {
-        print("[SUCCESS]");
-    }
-} catch (e) {
-}
-`;
+            var script =
+                prelude + '\n' +
+                'var evalcode = ' + JSON.stringify(evalcode) + ';\n' +
+                'try {\n' +
+                '    var result = eval(evalcode);\n' +
+                '    if (result) {\n' +
+                '        print("[SUCCESS]");\n' +
+                '    }\n' +
+                '} catch (e) {\n' +
+                '}\n';
 
             fs.writeFileSync('graalvmtest.js', script);
-            const actual = executeTestScript('graalvmtest.js', parents[0]);
+            var actual = executeTestScript('graalvmtest.js', parents[0]);
             testCount++;
             if (actual) {
                 testSuccess++;
@@ -182,52 +181,52 @@ try {
             if (test.res) {
                 // Take expected result from newest GraalVM version not newer
                 // than current version.
-                let expect;
-                for (const k of graalvmKeyList) {
+                var expect;
+                graalvmKeyList.forEach(function (k) {
                     if (test.res[k] !== undefined) {
                         expect = test.res[k];
                     }
-                }
+                });
 
                 if (resultsMatch(expect, actual)) {
                     // Matches.
                 } else if (expect === undefined) {
                     testOutOfDate++;
-                    console.log(`${testPath}: test result missing, res: ${JSON.stringify(expect)}, actual: ${JSON.stringify(actual)}`);
+                    console.log(testPath + ': test result missing, res: ' + JSON.stringify(expect) + ', actual: ' + JSON.stringify(actual));
                 } else {
                     testOutOfDate++;
-                    console.log(`${testPath}: test result out of date, res: ${JSON.stringify(expect)}, actual: ${JSON.stringify(actual)}`);
+                    console.log(testPath + ': test result out of date, res: ' + JSON.stringify(expect) + ', actual: ' + JSON.stringify(actual));
                 }
             } else {
                 testOutOfDate++;
-                console.log(`${testPath}: test.res missing`);
+                console.log(testPath + ': test.res missing');
             }
         }
     }
     if (test.subtests) {
-        const newParents = parents.slice(0);
+        var newParents = parents.slice(0);
         newParents.push(test.name);
-        for (const subtest of test.subtests) {
+        test.subtests.forEach(function (subtest) {
             runTest(newParents, subtest);
-        }
+        });
     }
 }
 
-for (const filename of fs.readdirSync('.')) {
-    const datafile = /^(data-.*)\.js$/.exec(filename);
+fs.readdirSync('.').forEach(function (filename) {
+    var datafile = /^(data-.*)\.js$/.exec(filename);
     if (!datafile) {
-        continue;
+        return;
     }
-    const suitename = datafile[1];
+    var suitename = datafile[1];
 
     console.log('');
-    console.log(`**** ${suitename} ****`);
+    console.log('**** ' + suitename + ' ****');
     console.log('');
-    const testsuite = require('./' + suitename);
-    for (const test of testsuite.tests) {
+    var testsuite = require('./' + suitename);
+    testsuite.tests.forEach(function (test) {
         runTest([ suitename ], test);
-    }
-}
+    });
+});
 
-console.log(`${testCount} tests executed: ${testSuccess} success, ${(testCount - testSuccess)} fail`);
-console.log(`${testOutOfDate} tests are out of date (data-*.js file .res)`);
+console.log(testCount + ' tests executed: ' + testSuccess + ' success, ' + (testCount - testSuccess) + ' fail');
+console.log(testOutOfDate + ' tests are out of date (data-*.js file .res)');
